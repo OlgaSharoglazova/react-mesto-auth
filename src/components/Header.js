d4c630cacd680a18282131d80d80948e67a2f6aa
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-06-25 16:31:31 +0800

    хедер стили

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 function Header() {
   return (
     <header className="header">
       <div className="header__logo" />
       <div className="header__navigation">
-        <Link to="/sign-up" className="header__link">Регистрация</Link>
-        <Link to="/sign-in" className="header__link">Войти</Link>
-        <Link to="/sign-in" className="header__link">Выйти</Link>
         <p className="header__email">email@mail.com</p>
+        <Link to="/sign-in" className="header__link">Выйти</Link>
       </div>
     </header>
   );
