bfa2521e9ba3dfaf2b9f78e83958c95ef6d806b1
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-06-29 16:20:37 +0800

    ссылки в шапке

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,46 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 function Header({ userData }) {
+  const location = useLocation();
 
-const navigate = useNavigate();
-function signOut() {
-  localStorage.removeItem("jwt");
-  navigate("/signin");
-}
-  
+  const navigate = useNavigate();
+
+  function signOut() {
+    localStorage.removeItem("jwt");
+    navigate("/signin");
+  }
+
   return (
     <header className="header">
       <div className="header__logo" />
       <ul className="header__navigation">
-        <li><p className="header__email">{userData.data?.email}</p></li>
-        <li><button onClick={signOut} className="header__button">Выйти</button></li>
-        <li></li>
-        <li></li>   
+        {location.pathname === "/" && (
+          <li>
+            <p className="header__email">{userData.data?.email}</p>
+          </li>
+        )}
+        {location.pathname === "/" && (
+          <li>
+            <button onClick={signOut} className="header__button">
+              Выйти
+            </button>
+          </li>
+        )}
+        {location.pathname === "/signup" && (
+          <li>
+            <Link to="/signin" className="header__link">
+              Войти
+            </Link>
+          </li>
+        )}
+        {location.pathname === "/signin" && (
+          <li>
+            <Link to="/signup" className="header__link">
+              Регистрация
+            </Link>
+          </li>
+        )}
       </ul>
     </header>
   );
